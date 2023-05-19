e5f60f918db1a7e48fdef6fe8e81589915389afa
RZorkin <zorkin81@gmail.com>
2023-05-19 20:45:16 +0300

    Add project files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
+import React, { useState } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Notification } from './Notification/Notification';
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
+
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleClick = e => {
+    const { name } = e.target;
+    switch (name) {
+      case 'good':
+        setGood(prevGood => prevGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevNeutral => prevNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    let totalFeedback = good + neutral + bad;
+    return totalFeedback ? Math.round((100 * good) / totalFeedback) : 0;
+  };
+
+  const options = ['good', 'neutral', 'bad'];
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions options={options} onLeaveFeedback={handleClick} />
+      </Section>
+      <Section title={'Statistics'}>
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification title={'There is no feedback'} />
+        )}
+      </Section>
+    </>
   );
-};
+}
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import css from './Notification.module.css';
+
+export const Notification = ({ title }) => {
+  return (
+    <>
+      <p className={css.message}>{title}</p>
+    </>
+  );
+};
+
+Notification.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
